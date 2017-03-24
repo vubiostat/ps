b0ab01dc633dbc315c14bb2a8af699e050dae6ee
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-03-24 14:51:56 -0500

    Remove conditional panels for sliders

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -16,23 +16,17 @@ fluidPage(
             inputId = "ssAlpha", label = "Type I error (α)",
             min = 0, max = 1, value = 0, step = 0.05
           ),
-          conditionalPanel("input.ssAlpha > 0",
-            sliderInput(
-              inputId = "ssPower", label = "Power",
-              min = 0, max = 1, value = 0, step = 0.05
-            ),
-            conditionalPanel("input.ssPower > input.ssAlpha",
-              sliderInput(
-                inputId = "ssDelta", label = "Difference in population means (δ)",
-                min = 0, max = 50, value = 0, step = 1
-              ),
-              conditionalPanel("input.ssDelta > 0",
-                sliderInput(
-                  inputId = "ssSigma", label = "Standard deviation (σ)",
-                  min = 0, max = 100, value = 0, step = 1
-                )
-              )
-            )
+          sliderInput(
+            inputId = "ssPower", label = "Power",
+            min = 0, max = 1, value = 0, step = 0.05
+          ),
+          sliderInput(
+            inputId = "ssDelta", label = "Difference in population means (δ)",
+            min = 0, max = 50, value = 0, step = 1
+          ),
+          sliderInput(
+            inputId = "ssSigma", label = "Standard deviation (σ)",
+            min = 0, max = 100, value = 0, step = 1
           )
         ),
         tabPanel("Power"),
