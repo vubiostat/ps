e6d9f752c2c960eee7771657f3d83e2cafc08baf
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-03-28 10:42:40 -0500

    Create sidebar form
    - create TTest class

--- inst/angular/src/app/app.component.ts
+++ inst/angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { TTest } from './t-test';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PS: Power and Sample Size Calculation';
+  ttest = new TTest();
 }
---
+++ inst/angular/src/app/t-test.ts
@@ -0,0 +1,9 @@
+export class TTest {
+  output: string;
+  design = "paired";
+  alpha: number;
+  power: number;
+  caseMean: number;
+  controlMean: number;
+  sigma: number;
+}
