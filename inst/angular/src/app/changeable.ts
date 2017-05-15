55460c20fe21c2085436164b9620a2e424abca6d
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-05-15 11:40:19 -0500

    Create ChangeEmitter class and Changeable decorator

---
+++ inst/angular/src/app/changeable.ts
@@ -0,0 +1,31 @@
+import { EventEmitter } from '@angular/core';
+
+export abstract class ChangeEmitter {
+  protected changes = {};
+  protected noEmit = false;
+  onChange = new EventEmitter();
+
+  protected emit(): void {
+    if (!this.noEmit && this.changes && Object.keys(this.changes).length > 0) {
+      let changes = Object.assign({}, this.changes);
+      this.changes = {};
+      this.onChange.emit(changes);
+    }
+  }
+}
+
+export function Changeable(target: Object, name: string) {
+  Object.defineProperty(target, name, {
+    get: function() {
+      return this[`_${name}`];
+    },
+    set: function(value) {
+      if (value != this[`_${name}`]) {
+        this[`_${name}`] = value;
+        this.changes[name] = value;
+        this.emit();
+      }
+    },
+    enumerable: true
+  });
+}
