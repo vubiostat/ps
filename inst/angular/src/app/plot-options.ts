fb9f4ec9f0938503f4c729fba4c3041e34b262b5
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-05-15 11:42:55 -0500

    Rename GlobalPlotOptions to PlotOptions, inherit from ChangeEmitter

--- inst/angular/src/app/plot-options.ts
+++ inst/angular/src/app/plot-options.ts
@@ -1,7 +1,29 @@
-export class GlobalPlotOptions {
-  height: number;
-  width: number;
-  fontFamily: string = "";
-  fontSize: number = 1;
-  lineWidth: number = 2;
+import { ChangeEmitter, Changeable } from './changeable';
+
+export class PlotOptions extends ChangeEmitter {
+  @Changeable height: number;
+  @Changeable width: number;
+  @Changeable fontFamily: string;
+  @Changeable fontSize: number;
+  @Changeable lineWidth: number;
+
+  constructor() {
+    super();
+    this.noEmit = true;
+    this.fontFamily = "";
+    this.fontSize = 1;
+    this.lineWidth = 2;
+    this.noEmit = false;
+    this.changes = {};
+  }
+
+  attributes(): any {
+    return({
+      height: this.height,
+      width: this.width,
+      fontFamily: this.fontFamily,
+      fontSize: this.fontSize,
+      lineWidth: this.lineWidth
+    });
+  }
 }
