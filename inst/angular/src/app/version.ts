1b7590fa991c288f318c623efc07a1d05a3e94a4
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-05-22 17:47:13 -0500

    Update version, rebuild dist

--- inst/angular/src/app/version.ts
+++ inst/angular/src/app/version.ts
@@ -1,3 +1,3 @@
-let commitHash: string = "5f627711b972d1154d2f851ecb1643ffa8052d1e";
-let buildTimestamp: string = "2018-05-15T17:01:20-05:00";
+let commitHash: string = "da04ff341e6941d7bee30194dd47837d8bf42171";
+let buildTimestamp: string = "2018-05-22T17:43:13-05:00";
 export { commitHash, buildTimestamp };
