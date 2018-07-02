69fd34cf62182dd7118a2c3e1d55474a78e137f3
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-07-02 11:57:57 -0500

    Fix interpretation text for independent t-tests

--- inst/angular/src/app/t-test/t-test.ts
+++ inst/angular/src/app/t-test/t-test.ts
@@ -88,40 +88,87 @@ export class TTest {
     let alpha = this.alpha.toFixed(2);
     let sigma = this.sigma.toFixed(2);
     let delta = this.delta.toFixed(2);
-    let n = Math.ceil(this.n);
+    let n = Math.ceil(this.n).toString();
     let power = this.power.toFixed(2);
-    let result;
-    if (this.output == 'n' || this.output == 'nByCI') {
-      result = `
-        We are planning a study of a continuous response variable from matched
-        pairs of study subjects. Prior data indicate that the difference in the
-        response of matched pairs is normally distributed with standard
-        deviation ${sigma}. If the true difference in the mean response of
-        matched pairs is ${delta}, we will need to study ${n} pairs of subjects
-        to be able to reject the null hypothesis that this response difference
-        is zero with probability (power) ${power}. The Type I error probability
-        associated with this test of this null hypothesis is ${alpha}.
-      `;
-    } else if (this.output == 'power') {
-      result = `
-        We are planning a study with ${n} pairs of subjects. Prior data
-        indicate that the difference in the response of matched pairs is
-        normally distributed with standard deviation ${sigma}. If the true
-        difference in the mean response of matched pairs is ${delta}, we will
-        be able to reject the null hypothesis that this response difference is
-        zero with probability (power) ${power}. The Type I error probability
-        associated with this test of this null hypothesis is ${alpha}.
-      `;
-    } else if (this.output == 'delta') {
-      result = `
-        We are planning a study with ${n} pairs of subjects. Prior data
-        indicate that the difference in the response of matched pairs is
-        normally distributed with standard deviation ${sigma}. We will be able
-        to detect a true difference in the mean response of matched pairs of
-        -${delta} or ${delta} with probability (power) ${power}. The Type I
-        error probability associated with this test of the null hypothesis that
-        this response difference is zero is ${alpha}.
-      `;
+    let m = '', mn = '';
+    if (this.m) {
+      m = this.m.toFixed(2);
+      mn = Math.ceil(this.m * this.n).toString();
+    }
+
+    let result = '';
+    if (this.kind === TTestKind.Paired) {
+      if (this.output == 'n' || this.output == 'nByCI') {
+        result = `
+          We are planning a study of a continuous response variable from matched
+          pairs of study subjects. Prior data indicate that the difference in the
+          response of matched pairs is normally distributed with standard
+          deviation ${sigma}. If the true difference in the mean response of
+          matched pairs is ${delta}, we will need to study ${n} pairs of subjects
+          to be able to reject the null hypothesis that this response difference
+          is zero with probability (power) ${power}. The Type I error probability
+          associated with this test of this null hypothesis is ${alpha}.
+        `;
+      } else if (this.output == 'power') {
+        result = `
+          We are planning a study with ${n} pairs of subjects. Prior data
+          indicate that the difference in the response of matched pairs is
+          normally distributed with standard deviation ${sigma}. If the true
+          difference in the mean response of matched pairs is ${delta}, we will
+          be able to reject the null hypothesis that this response difference is
+          zero with probability (power) ${power}. The Type I error probability
+          associated with this test of this null hypothesis is ${alpha}.
+        `;
+      } else if (this.output == 'delta') {
+        result = `
+          We are planning a study with ${n} pairs of subjects. Prior data
+          indicate that the difference in the response of matched pairs is
+          normally distributed with standard deviation ${sigma}. We will be able
+          to detect a true difference in the mean response of matched pairs of
+          -${delta} or ${delta} with probability (power) ${power}. The Type I
+          error probability associated with this test of the null hypothesis that
+          this response difference is zero is ${alpha}.
+        `;
+      }
+    } else if (this.kind === TTestKind.Independent) {
+      if (this.output == 'n' || this.output == 'nByCI') {
+        result = `
+          We are planning a study of a continuous response variable from
+          independent control and experimental subjects with ${m} control(s)
+          per experimental subject.  In a previous study the response within
+          each subject group was normally distributed with standard deviation
+          ${sigma}.  If the true difference in the experimental and control
+          means is ${delta}, we will need to study ${n} experimental subjects
+          and ${mn} control subjects to be able to reject the null hypothesis
+          that the population means of the experimental and control groups are
+          equal with probability (power) ${power}.   The Type I error
+          probability associated with this test of this null hypothesis is
+          ${alpha}.
+        `;
+      } else if (this.output == 'power') {
+        result = `
+          We are planning a study with ${n} experimental subjects and ${mn}
+          control subjects.  In a previous study the response within each
+          subject group was normally distributed with standard deviation
+          ${sigma}.  If the true difference in the experimental and control
+          means is ${delta}, we will be able to reject the null hypothesis that
+          the population means of the experimental and control groups are equal
+          with probability (power) ${power}.   The Type I error probability
+          associated with this test of this null hypothesis is ${alpha}.
+        `;
+      } else if (this.output == 'delta') {
+        result = `
+          We are planning a study with ${n} experimental subjects and ${mn}
+          control subjects.  In a previous study the response within each
+          subject group was normally distributed with standard deviation
+          ${sigma}.  We will be able to detect a true difference in the mean
+          response of experimental and control subjects of -${delta} or
+          ${delta} with probability (power) ${power}.   The Type I error
+          probability associated with this test of the null hypothesis that the
+          population means of the experimental and control groups are equal is
+          ${alpha}.
+        `;
+      }
     }
     return result.replace(/\s+/g, " ").trim();
   }
