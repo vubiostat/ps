a0fab667d8817e630825fdd9b4e64e296d01cb62
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2020-06-30 14:26:23 -0500

    Rebuild dist

--- inst/angular/src/app/version.ts
+++ inst/angular/src/app/version.ts
@@ -1,3 +1,3 @@
-let commitHash: string = "009a67b6b3f47d4d8a0020f3bd313a15acb8ec4c";
-let buildTimestamp: string = "2020-06-09T17:35:31-05:00";
+let commitHash: string = "6055f91ad58d60467e6ebcc225e4074cf319eb9c";
+let buildTimestamp: string = "2020-06-30T14:19:09-05:00";
 export { commitHash, buildTimestamp };
