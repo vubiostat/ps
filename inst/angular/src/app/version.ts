f6dd3eca353d47564967f5d7537270905e044683
Dale Plummer <dale.plummer@vumc.org>
2021-02-10 15:14:28 -0600

    after npm run build

--- inst/angular/src/app/version.ts
+++ inst/angular/src/app/version.ts
@@ -1,3 +1,3 @@
-let commitHash: string = "2da94e91f6f97ecf9ac701157004d9bb87f38050";
-let buildTimestamp: string = "2020-12-22T11:57:10-06:00";
+let commitHash: string = "63d13b9f6a38b766e88cf954db4ea51e25f78b68";
+let buildTimestamp: string = "2021-02-10T15:09:43-06:00";
 export { commitHash, buildTimestamp };
