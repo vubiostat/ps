e4df1496e1d84531138ff4f5f9b552a7b92fa299
Dale Plummer <dale.plummer@vumc.org>
2021-02-12 10:10:49 -0600

    some stuff I missed in yesterday's commit

--- inst/angular/src/app/version.ts
+++ inst/angular/src/app/version.ts
@@ -1,3 +1,3 @@
-let commitHash: string = "63d13b9f6a38b766e88cf954db4ea51e25f78b68";
-let buildTimestamp: string = "2021-02-10T15:09:43-06:00";
+let commitHash: string = "f6dd3eca353d47564967f5d7537270905e044683";
+let buildTimestamp: string = "2021-02-10T15:14:28-06:00";
 export { commitHash, buildTimestamp };
